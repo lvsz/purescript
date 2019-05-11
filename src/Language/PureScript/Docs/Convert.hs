dd41ab620abd6921172cd51b01e64952bb4d7463
Harry Garrood <harry@garrood.me>
2019-05-11 18:45:52 +0100

    Don't export unnecessary functions

--- src/Language/PureScript/Docs/Convert.hs
+++ src/Language/PureScript/Docs/Convert.hs
@@ -3,10 +3,8 @@
 
 module Language.PureScript.Docs.Convert
   ( convertModules
-  , convertModulesWithEnv
   , convertTaggedModulesInPackage
   , convertModulesInPackage
-  , convertModulesInPackageWithEnv
   , convertModule
   ) where
 
